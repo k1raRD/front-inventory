e2f13ace88c840cfb51be677632ef5217901f2d1
Alex collado <Alexanderyt777@gmail.com>
2023-09-08 02:58:32 -0400

    Initial commit for front inventory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './modules/dashboard/pages/dashboard.component';
+import { HomeComponent } from './modules/dashboard/components/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    DashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
